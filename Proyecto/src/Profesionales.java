ad43c46695407d0fdcd9dcd72456363ab9121bd2
NaomieC99 <102881223+NaomieC99@users.noreply.github.com>
2022-04-02 21:37:36 -0300

    Entrega EP1

--- Proyecto/src/Profesionales.java
+++ Proyecto/src/Profesionales.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 
 public class Profesionales {
+    
     private String Nombre; //coresponde al nombre del profesional
     private int Rut; //corresponde al rut del profesional para poder identificarlo como persona individual
     private String Especialidad; // se le asigna un codigo de especialidad según corresponda
@@ -9,13 +10,14 @@ public class Profesionales {
     private double horario; // el horario muestra la última hora disponible del profesional, se considera que el horaio de inicio de atención es desde las 8am.
     private Profesionales profesional;
     private ArrayList <Profesionales>  profesionales;
-    
+    /**
+     *
+     */
     
     //constructores
     public Profesionales(){
         
     }
-    
     
     public Profesionales(String Nombre, int Rut,int Telefono, ArrayList agenda, double horario, Profesionales profesionaels ){
         this.Nombre = Nombre;
@@ -24,17 +26,43 @@ public class Profesionales {
         this.horario = 0;
         this.profesionales = new ArrayList <>();  
     }   
-     
+  
     
-    public void mostrarProfesionales(Profesionales Dato){   
-        System.out.println("Nombre: "+Dato.Nombre);
-        System.out.println("Rut: "+Dato.Rut);
-        System.out.println("Especialidad: "+Dato.Especialidad);
-        System.out.println("Horario: "+Dato.horario);
-    }    
     
+    
+    
+    //Accesores y mutadores - getter y setters
 
-    //Este metodo busca en el arreglo de profesionales a cada profesional que cuente con el codigo 
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public int getRut() {
+        return Rut;
+    }
+
+    public String getEspecialidad() {
+        return Especialidad;
+    }
+
+    public ArrayList getAgenda() {
+        return agenda;
+    }
+
+    public double getHorario() {
+        return horario;
+    }
+
+    public Profesionales getProfesional() {
+        return profesional;
+    }
+
+    public ArrayList<Profesionales> getProfesionales() {
+        return profesionales;
+    }
+    
+    
+        //Este metodo busca en el arreglo de profesionales a cada profesional que cuente con el codigo 
     //de especialidad requerido y muetra los días disponibles que tiene para atender
     public void buscarEspecialidad(String codigo){
         for (int i= 0; i>= profesionales.size(); i++){         
@@ -45,63 +73,43 @@ public class Profesionales {
         }
     }
     
-    //Metodo para mostrar el horario de atencion disponible para cada profesional
+        //Metodo para mostrar el horario de atencion disponible para cada profesional
     public void mostrarHorario(int i){
         System.out.println("A continuación se muestran las horas disponibles para este día");
     }
 
-    
-    //Accesores y mutadores - getter y setters
-    public String getNombre(){
-        return Nombre;
-    }
-    
-    public int getRut(){
-        return Rut;
-    }
-    
-    public String getEspecialidad(){
-        return Especialidad;
-    }
-    
-    public void setNombre(String Nombre){
-        Nombre=Nombre;
-    }
-    
-    public void setApellido(String apellido){
-        apellido=apellido;
-    }
-    
-    public void setRut(int Rut){
-        Rut=Rut;
-    }
-    
-    public void setEspecialidad(String Especialidad){
-        Especialidad=Especialidad;
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
     }
 
-    public ArrayList getProfresionales() {
-        return profesionales;
+    public void setRut(int Rut) {
+        this.Rut = Rut;
     }
 
-    public void setProfresionales(ArrayList profresionales) {
-        this.profesionales = profesionales;
-    }
-
-    public ArrayList getAgenda() {
-        return agenda;
+    public void setEspecialidad(String Especialidad) {
+        this.Especialidad = Especialidad;
     }
 
     public void setAgenda(ArrayList agenda) {
         this.agenda = agenda;
     }
 
-    public double getHorario() {
-        return horario;
-    }
-
     public void setHorario(double horario) {
         this.horario = horario;
     }
+
+    public void setProfesional(Profesionales profesional) {
+        this.profesional = profesional;
+    }
+
+    public void setProfesionales(ArrayList<Profesionales> profesionales) {
+        this.profesionales = profesionales;
+    }
+
+   
+    
+    
     
 }
+ 
