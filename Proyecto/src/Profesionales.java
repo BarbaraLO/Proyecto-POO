16bb8efbf5ba52fcff1bdf7408692817a4bf6cd7
BarbaraLO <89320680+BarbaraLO@users.noreply.github.com>
2022-04-02 23:08:12 -0300

    Corrección de tipo en variable especialidad int

--- Proyecto/src/Profesionales.java
+++ Proyecto/src/Profesionales.java
@@ -5,7 +5,7 @@ public class Profesionales {
     
     private String Nombre; //coresponde al nombre del profesional
     private int Rut; //corresponde al rut del profesional para poder identificarlo como persona individual
-    private String Especialidad; // se le asigna un codigo de especialidad según corresponda
+    private int Especialidad; // se le asigna un codigo de especialidad según corresponda
     private ArrayList agenda; // agenda trabaja con codigos del 1 al 6 representando los días de la semana, esto para tener una idea de los días disponibles del profesional
     private double horario; // el horario muestra la última hora disponible del profesional, se considera que el horaio de inicio de atención es desde las 8am.
     private Profesionales profesional;
@@ -19,14 +19,14 @@ public class Profesionales {
         
     }
     
-    public Profesionales(String nombre, int rut, String especialidad, double horario) {
+    public Profesionales(String nombre, int rut, int especialidad, double horario) {
         this.nombre = nombre;
         this.rut = rut;
         this.especialidad = especialidad;
         this.horario = horario;
     }    
     
-        public Profesionales(String Nombre, int Rut,int Telefono, ArrayList agenda, double horario, Profesionales profesionaels ){
+        public Profesionales(String Nombre, int Rut, ArrayList agenda, double horario, Profesionales profesionales ){
         this.Nombre = Nombre;
         this.Rut = Rut;
         this.agenda = new ArrayList();
@@ -96,7 +96,7 @@ public class Profesionales {
     
         //Este metodo busca en el arreglo de profesionales a cada profesional que cuente con el codigo 
     //de especialidad requerido y muetra los días disponibles que tiene para atender
-    public void buscarEspecialidad(String codigo){
+    public void buscarEspecialidad(int codigo){
         for (int i= 0; i>= profesionales.size(); i++){         
             if ( profesionales.get(i).getEspecialidad().equals(codigo)){
                 System.out.println("El profesional "+ profesionales.get(i).Nombre + "Cuenta con los siguientes días de atención disponible: " );             
@@ -112,10 +112,10 @@ public class Profesionales {
 
     
 public void DatosProfesionales(){     
-        profesionales.add(new Profesionales("Miguel Sanchez",114672852,"Cardiologia",555));
-        profesionales.add(new Profesionales("Jose Gonzalez",96482745,"Dermatologia",666));
-        profesionales.add(new Profesionales("Marcela Araya",79472487,"Pediatria",777));
-        profesionales.add(new Profesionales("Sofia Rodriguez",158593642,"Neurologia",888));
+        profesionales.add(new Profesionales("Miguel Sanchez",114672852,01,555)); // Cardiologia
+        profesionales.add(new Profesionales("Jose Gonzalez",96482745,02,666)); // Dermatologia
+        profesionales.add(new Profesionales("Marcela Araya",79472487,03,777)); // Pediatria
+        profesionales.add(new Profesionales("Sofia Rodriguez",158593642,04,888)); // Neurologia
     }
     
         public void mostrarProfesionales(){   
