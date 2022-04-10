041f08b7fb67f2bf86808900754a3d49e6f3a460
BarbaraLO <89320680+BarbaraLO@users.noreply.github.com>
2022-04-09 21:07:22 -0400

    Update Principal.java

--- Proyecto/src/Principal.java
+++ Proyecto/src/Principal.java
@@ -12,49 +12,61 @@ import java.util.Scanner;
 public class Principal {
 
     public static Pacientes pacientes = new Pacientes();
-    public static Profesionales profesional = new Profesionales();
+    public static Profesionales profesionales = new Profesionales();
 
     public static void main(String[] args) throws ParseException {
         Scanner Entrada;
         int opcion;
         do {
+            System.out.println("____________________________");
             System.out.println("Ingrese una opcion");
-            System.out.println("1- Mostrar especialidades");
-            System.out.println("2- Registrar hora");
-            System.out.println("3- Mostrar hora del paciente");
-            System.out.println("4- Cambiar hora");
-            System.out.println("5- Eliminar hora");
-            System.out.println("6- Salir");
+            System.out.println("____________________________");
+            System.out.println("1- Registrar paciente");
+            System.out.println("2- Mostrar listado de pacientes");
+            System.out.println("3- Mostrar listado especialidades");
+            System.out.println("4- Mostrar listado de profesionales");
+            System.out.println("5- Mostrar hora del paciente");
+            System.out.println("6- Cambiar hora");
+            System.out.println("7- Eliminar hora");
+            System.out.println("8- Salir");
+            System.out.println("____________________________");
 
             Entrada = new Scanner(System.in);
             opcion = Entrada.nextInt();
 
             switch (opcion) {
                 case 1:
-                    profesional.mostrar();
+                    pacientes.AgregarPaciente();
                     break;
 
                 case 2:
-                    pacientes.validarPaciente();
+                    pacientes.mostrarPacientes();
                     break;
 
                 case 3:
-                    pacientes.mostrarHora();
+                    profesionales.mostrar();
                     break;
 
                 case 4:
-                    pacientes.cambiarHora();
-
+                    profesionales.DatosProfesionales();
+                    profesionales.mostrarProfesionales();
                     break;
 
                 case 5:
                     pacientes.eliminarHora();
+                    pacientes.mostrarHora();
+                    break;
+                case 6:
+                    pacientes.cambiarHora();
 
                     break;
+                case 7:
+                    pacientes.cambiarHora();
 
+                    break;                    
             }
 
-        } while (opcion != 6);
+        } while (opcion != 8);
     }
 
 }
