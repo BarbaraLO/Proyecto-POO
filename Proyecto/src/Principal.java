c740bbf21f7a21cce777ec0afa2143eac78bbd88
NaomieC99 <102881223+NaomieC99@users.noreply.github.com>
2022-05-21 21:11:22 -0400

    Update Principal.java
    agregación del método buscar

--- Proyecto/src/Principal.java
+++ Proyecto/src/Principal.java
@@ -23,58 +23,63 @@ public class Principal {
             System.out.println("INGRESE UNA OPCION:");
             System.out.println("____________________________");
             System.out.println("1- Registrar paciente");
-            System.out.println("2- Modificar Paciente");
-            System.out.println("3- Eliminar Paciente");
-            System.out.println("4- Mostrar listado de pacientes");
-            System.out.println("5- Mostrar listado de profesionales y especialidad");
-            System.out.println("6- Reservar hora");
-            System.out.println("7- Modificar Reserva");
-            System.out.println("8- Eliminar reserva");
-            System.out.println("9- Mostrar reservas");
+            System.out.println("2- Buscar paciente");
+            System.out.println("3- Modificar Paciente");
+            System.out.println("4- Eliminar Paciente");
+            System.out.println("5- Mostrar listado de pacientes");
+            System.out.println("6- Mostrar listado de profesionales y especialidad");
+            System.out.println("7- Reservar hora");
+            System.out.println("8- Modificar Reserva");
+            System.out.println("9- Eliminar reserva");
+            System.out.println("10- Mostrar reservas");
             System.out.println("0- Salir");
             System.out.println("____________________________");
 
             Entrada = new Scanner(System.in);
             opcion = Entrada.nextInt();
             
-            switch (opcion) {
+              switch (opcion) {
                 case 1:
                     pacientes.AgregarPaciente();
                     break;
-
                 case 2:
-                    pacientes.ModificarPaciente(pacientes);
+                    pacientes.buscar();
                     break;
 
                 case 3:
-                    pacientes.EliminarPaciente(pacientes);
+                    pacientes.ModificarPaciente(pacientes);
                     break;
 
                 case 4:
-                    pacientes.mostrar();
+                    pacientes.EliminarPaciente(pacientes);
                     break;
 
                 case 5:
+                    pacientes.mostrar();
+                    break;
+
+                case 6:
                     profesionales.mostrar();
                     profesionales.DatosProfesionales();
                     profesionales.mostrarProfesionales();
                     break;
                     
-                case 6:
+                case 7:
                     reservas.InsertarDatos(pacientes);
                     break;
                     
-                case 7:
+                case 8:
                     reservas.ModificarReserva();
                     break;
                     
-                case 8:
+                case 9:
                     reservas.EliminarReserva();
                     break;
                     
-                case 9:
+                case 10:
                     reservas.ImprimirDatos();
                     break;
+                    
             }
 
         } while (opcion != 0);
